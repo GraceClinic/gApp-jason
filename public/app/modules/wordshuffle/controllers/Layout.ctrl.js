86a40cab4615d5536571db7f545a31edd84c779b
ashish701ranjan <ashish701ranjan@gmail.com>
2016-09-22 00:00:44 +0530

    ui-bootstrap => 0.13.0, popoverToggle, closePopoverDirective added

--- public/app/modules/wordshuffle/controllers/Layout.ctrl.js
+++ public/app/modules/wordshuffle/controllers/Layout.ctrl.js
@@ -5,8 +5,56 @@
      * @param    {App_Common_Models_SysMan}  SysMan  - reference to the SysMan singleton
      * @this        WordShuffle_Controllers_Layout
      */
-    function WordShuffle_Controllers_Layout(SysMan) {
+    function WordShuffle_Controllers_Layout(SysMan, Player, Game, $state) {
         var self = this;		// required alias to address resolution to immediate scope
+        
+        /**
+         * @property    Player
+         * this will give access to the player model - required for a popover
+         *
+         * @type    {WordShuffle_Models_Player}
+         * @public
+         **/
+        Object.defineProperty(self,'Player',{get: getPlayer, set: setPlayer, enumerable:true});
+        function getPlayer(){
+            return Player;
+        }
+        function setPlayer(value){
+            self.SysMan.Logger.entry('Player.set() not allowed!',self.constructor.name,self.SysMan.Logger.TYPE.ERROR,self.SysMan.Logger.ERRNO.CTRL_ERROR);
+        }
+
+        /**
+         * @property    Game
+         * access Game, set newGame to be true on click of popover
+         *
+         * @type    {WordShuffle_Models_Game}
+         * @public
+         **/
+        Object.defineProperty(self,'Game',{get: getGame, set: setGame, enumerable:true});
+        var _Game;
+        function getGame(){
+            return Game;
+        }
+        function setGame(value){
+            self.SysMan.Logger.entry('Game.set() not allowed!',self.constructor.name,self.SysMan.Logger.TYPE.ERROR,self.SysMan.Logger.ERRNO.CTRL_ERROR);
+        }
+        
+        /**
+         * @property    popUp
+         * set this to true/false, allowing the popover to functionally appear and disappear
+         *
+         * @type    {boolean}
+         * @public
+         **/
+        Object.defineProperty(self,'popUp',{get: getPopUp, set: setPopUp, enumerable:true});
+        var _popUp = false;
+        function getPopUp(){
+            return _popUp;
+        }
+        function setPopUp(value){
+            _popUp = value;
+        }
+        
 
         /**
          * @method   isActive
@@ -32,7 +80,10 @@
     WordShuffle_Controllers_Layout.prototype.constructor = WordShuffle_Controllers_Layout;
 
     WordShuffle_Controllers_Layout.$inject = [
-        'App_Common_Models_SysMan'
+        'App_Common_Models_SysMan',
+        'WordShuffle_Models_Player',
+        'WordShuffle_Models_Game',
+        '$state'
     ];
 
     angular.module('App_WordShuffle').controller('WordShuffle_Controllers_Layout', WordShuffle_Controllers_Layout);
